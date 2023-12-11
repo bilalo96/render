9b93320bb17fdb4419d94a2048acb1def8906494
bilalo96 <140337206+bilalo96@users.noreply.github.com>
2023-12-11 08:07:54 +0300

    Update celery.py

--- project/celery.py
+++ project/celery.py
@@ -1,9 +1,19 @@
 # django_celery/celery.py
 
+from __future__ import absolute_import
 import os
 from celery import Celery
+from django.conf import settings
+
+
+
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 app = Celery("project")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request:{0!r}'.format(self.request))
