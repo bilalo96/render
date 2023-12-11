3dcf9d205c60b64d6b78c73759bc18d9ab120d0c
bilalo96 <140337206+bilalo96@users.noreply.github.com>
2023-12-11 08:07:09 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -167,16 +167,19 @@ DEFAULT_TO_EMAIL = 'recipient_email@example.com'
 
 EMAIL_BACKEND = 'django.core.email.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'bilalkwider40@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'qwiderbilal27@gmail.com'
+EMAIL_HOST_PASSWORD = 'babanwwncvnmunob'
 EMAIL_USE_TLS = True
 EMAIL_PORT =    '587'
-
 
 
 # Celery settings
 CELERY_BROKER_URL = "redis://localhost:6379"
 CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
 
 
 
