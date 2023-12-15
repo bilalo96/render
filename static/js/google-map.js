62b21f90c4992b61cb259314830956fd7456e8ab
bilalo96 <140337206+bilalo96@users.noreply.github.com>
2023-12-15 10:28:40 +0300

    Update google-map.js

--- static/js/google-map.js
+++ static/js/google-map.js
@@ -1,22 +1,13 @@
-
 var google;
 
 function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(25.184242 , 55.272430);
-    // 39.399872
-    // -8.224454
-    var map = new google.maps.Map(mapElement, mapOptions);
+    var myLatlng = new google.maps.LatLng(25.184242, 55.272430);
+
+    var mapElement = document.getElementById('map'); // Define mapElement first
+
     var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
         zoom: 7,
-
-        // The latitude and longitude to center the map (always required)
         center: myLatlng,
-
-        // How you would like to style the map. 
         scrollwheel: false,
         styles: [
             {
@@ -33,29 +24,23 @@ function init() {
             }
         ]
     };
-        new google.maps.Marker({
-        position :{lat:25.255901445315114,lng: 55.33936933429995},
-        map : map,
-        label:"A",
-        title : "SpeedSwichCarRental",
-        draggable:false,
-        animation:google.maps.Animation.BOUNCE,
-    })
 
-    
+    var map = new google.maps.Map(mapElement, mapOptions); // Create the map using mapElement and mapOptions
 
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
+    new google.maps.Marker({
+        position: { lat: 25.255901445315114, lng: 55.33936933429995 },
+        map: map,
+        label: "A",
+        title: "SpeedSwichCarRental",
+        draggable: false,
+        animation: google.maps.Animation.BOUNCE,
+    });
 
-    // Create the Google Map using out element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-    
     var addresses = ['Dubai'];
 
     for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
+        $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=' + addresses[x] + '&sensor=false', null, function (data) {
+            var p = data.results[0].geometry.location;
             var latlng = new google.maps.LatLng(p.lat, p.lng);
             new google.maps.Marker({
                 position: latlng,
@@ -65,6 +50,6 @@ function init() {
 
         });
     }
-    
 }
+
 google.maps.event.addDomListener(window, 'load', init);
